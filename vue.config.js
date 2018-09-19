87d5409189b86e6a1fa6019ea95aae7354c76c1c
Erik Vullings <erik.vullings@gmail.com>
2018-09-19 17:40:05 +0200

    Update github docs

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,17 @@
 module.exports = {
-    baseUrl: '/public/',
-    devServer: {
-        port: 2345
-    },
-    configureWebpack: {
-       
-        module: {
-            rules: [
-                {
-                    test: /\.html$/,
-                    loader: 'raw-loader',
-                    exclude: ['./public/index.html']
-                }
-            ]
+  baseUrl: '',
+  devServer: {
+    port: 2345
+  },
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          loader: 'raw-loader',
+          exclude: ['./public/index.html']
         }
+      ]
     }
+  }
 };
